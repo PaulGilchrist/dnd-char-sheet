d95e0651d92a4e858f924cf12ab60ad14d6037a1
paulgilchrist <PGilchrist@pulte.com>
2024-01-14 10:19:16 -0500

    Multi-character support

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # To Do
 
 * Magic Items
+* Load multiple characters at once into separate tabs
--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import CharSheet from './char-sheet'
 
 function App() {
+    const [activeCharacter, setActiveCharacter] = React.useState(null);
+    const [characters, setCharacters] = React.useState([]);
     const [classes, setClasses] = React.useState([]);
     const [equipment, setEquipment] = React.useState([]);
     const [spells, setSpells] = React.useState([]);
-    const [stats, setStats] = React.useState(null);
 
     React.useEffect(() => {
         fetch('/dnd-char-sheet/data/classes.json')
@@ -32,26 +33,42 @@ function App() {
             });
     }, []);
 
-    const handleButtonClick = () => {
+    const handleCharacterClick = (character) => {
+        setActiveCharacter(character);
+    }
+
+    const handleUploadClick = () => {
         const input = document.createElement('input');
         input.type = 'file';
         input.accept = '.json';
+        input.multiple = true;
         input.onchange = (event) => {
-            const file = event.target.files[0];
-            const reader = new FileReader();
-            reader.readAsText(file);
-            reader.onload = (event) => {
-                const data = JSON.parse(event.target.result);
-                setStats(data);
-            };
+            const files = event.target.files;
+            const readers = [];
+            for (let i = 0; i < files.length; i++) {
+                const reader = new FileReader();
+                reader.readAsText(files[i]);
+                readers.push(reader);
+            }
+            setActiveCharacter(null);
+            setCharacters([]);
+            Promise.all(
+                readers.map((reader) => reader.onload = (event) => {
+                    const data = JSON.parse(event.target.result);
+                    setCharacters((characters) => [...characters, data]);
+                })
+            );
         };
         input.click();
     }
-
+    if(characters.length > 0 && !activeCharacter) {
+        setActiveCharacter(characters[0]);
+    }
     return (
         <div>
-            {stats && <CharSheet allClasses={classes} allEquipment={equipment} allSpells={spells} playerStats={stats}></CharSheet>}
-            <button className="no-print" onClick={handleButtonClick}>Upload Character</button>
+            {characters.length > 0 && characters.map((character) => { return (<button key={character.name} className={`no-print ${activeCharacter && activeCharacter.name === character.name ? 'active' : ''}`} onClick={() => handleCharacterClick(character)}>{character.name}</button>) })}
+            <button className="inactive no-print" onClick={handleUploadClick}>Upload Characters</button>
+            {activeCharacter && <CharSheet allClasses={classes} allEquipment={equipment} allSpells={spells} playerStats={activeCharacter}></CharSheet>}
         </div>
     )
 }
