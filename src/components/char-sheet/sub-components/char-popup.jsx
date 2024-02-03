3bd6da5ad0ca336f7d9ee3c76008328fe0d103c1
paulgilchrist <PGilchrist@pulte.com>
2024-02-03 18:26:38 -0500

    Improve popup

--- src/components/char-sheet/sub-components/char-popup.jsx
+++ src/components/char-sheet/sub-components/char-popup.jsx
@@ -3,7 +3,9 @@ import './char-popup.css'
 
 function CharPopup({ html, onClick }) {
     return (
-        <div className="char-popup" dangerouslySetInnerHTML={{ __html: html }} onClick={onClick}></div>
+        <div className="char-popup-overlay">
+            <div className="char-popup-modal" dangerouslySetInnerHTML={{ __html: html }} onClick={onClick}></div>
+        </div>
     )
 }
 
