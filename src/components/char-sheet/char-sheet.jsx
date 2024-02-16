b9be1ad7ba9225df57bde4c14771a494d8e2fdfa
paulgilchrist <PGilchrist@pulte.com>
2024-02-16 16:05:48 -0500

    API support

--- src/components/char-sheet/char-sheet.jsx
+++ src/components/char-sheet/char-sheet.jsx
@@ -19,36 +19,41 @@ function CharSheet({ allAbilityScores, allClasses, allEquipment, allMagicItems, 
     const [playerStats, setPlayerStats] = React.useState(null);
     React.useEffect(() => {
         const fetchData = async () => {
+            const fullUrl = `http://${window.location.hostname}:3000/${utils.getFirstName(playerSummary.name)}/`;
+            // console.log(fullUrl)
+            let response = null;
             try {
-                const fullUrl = `http://${window.location.hostname}:3000/${utils.getFirstName(playerSummary.name)}/`;
-                // console.log(fullUrl)
-                const response = await fetch(fullUrl, {
+                response = await fetch(fullUrl, {
                     method: 'GET',
                     mode: 'cors'
                 });
-                if (response.ok) {
+            } catch(e) {
+                // console.log(e.message); 
+            }
+            if(response && response.ok) {
+                try {
                     const data = await response.json();
                     localStorage.setItem(playerSummary.name, JSON.stringify(data));
+                } catch(e) {
+                    // console.log(e.message); 
                 }
-                const stats = rules.getPlayerStats(allClasses, allEquipment, allMagicItems, allRaces, allSpells, playerSummary);
-                let preparedSpells = storage.get(stats.name, 'preparedSpells');
-                if (preparedSpells) {
-                    stats.spellAbilities.spells.forEach(spell => {
-                        if (preparedSpells.includes(spell.name)) {
-                            if (spell.prepared === '') {
-                                spell.prepared = 'Prepared';
-                            }
-                        } else {
-                            if (spell.prepared === 'Prepared') {
-                                spell.prepared = '';
-                            }
-                        }
-                    });
-                }
-                setPlayerStats(stats);
-            } catch (err) {
-                // console.log(err.message);
             }
+            const stats = rules.getPlayerStats(allClasses, allEquipment, allMagicItems, allRaces, allSpells, playerSummary);
+            let preparedSpells = storage.get(stats.name, 'preparedSpells');
+            if (preparedSpells) {
+                stats.spellAbilities.spells.forEach(spell => {
+                    if (preparedSpells.includes(spell.name)) {
+                        if (spell.prepared === '') {
+                            spell.prepared = 'Prepared';
+                        }
+                    } else {
+                        if (spell.prepared === 'Prepared') {
+                            spell.prepared = '';
+                        }
+                    }
+                });
+            }
+            setPlayerStats(stats);
         };
         fetchData();
     }, [allAbilityScores, allClasses, allEquipment, allMagicItems, allRaces, allSpells, playerSummary]);
