88311702efe840cba49767b86e95be076760000f
paulgilchrist <PGilchrist@pulte.com>
2026-04-25 17:24:38 -0400

    Fix null reference error in Fighter class component

--- src/components/char-sheet/char-summary/char-class-fighter.jsx
+++ src/components/char-sheet/char-summary/char-class-fighter.jsx
@@ -9,11 +9,13 @@ function CharClassFighter({ playerStats }) {
     const [indomitableUses, setIndomitableUses] = React.useState(0);
     const [showIndomitableUsesInput, setShowIndomitableUsesInput] = React.useState(false);
     React.useEffect(() => {
-        let actionSurges = storage.getProperty(playerStats.name, 'actionSurges');
-        setActionSurges(actionSurges ? actionSurges : classSpecific.action_surges);
-        let indomitableUses = storage.getProperty(playerStats.name, 'indomitableUses');
-        setIndomitableUses(indomitableUses ? indomitableUses : classSpecific.indomitable_uses);
-    }, [playerStats]);
+            const classSpecific = playerStats.class?.class_levels?.[playerStats.level - 1]?.class_specific;
+            if (!classSpecific) return;
+            let actionSurges = storage.getProperty(playerStats.name, 'actionSurges');
+            setActionSurges(actionSurges ? actionSurges : classSpecific.action_surges);
+            let indomitableUses = storage.getProperty(playerStats.name, 'indomitableUses');
+            setIndomitableUses(indomitableUses ? indomitableUses : classSpecific.indomitable_uses);
+         }, [playerStats]);
     const handleActionSurgesToggle = () => {
         setShowActionSurgesInput((showInput) => !showInput);
     };
@@ -28,22 +30,25 @@ function CharClassFighter({ playerStats }) {
         storage.setProperty(playerStats.name, 'indomitableUses', indomitableUses);
         setIndomitableUses(indomitableUses);
     };
-    const classSpecific = playerStats.class.class_levels[playerStats.level-1].class_specific;
-    return (<React.Fragment>
-        {playerStats.class.name === 'Fighter' && <div>
-            <div><b>Fighting Styles: </b>{playerStats.class.fightingStyles.join(', ')}</div>
-            {playerStats.class.subclass.maneuvers && <div>
-                <b>Maneuvers: </b>{playerStats.class.subclass.maneuvers.sort().join(', ')}
-            </div>}
-            <div><b>Extra Attacks: </b>{classSpecific.extra_attacks}</div>
-            <div className="clickable" onClick={handleActionSurgesToggle} onKeyDown={handleActionSurgesToggle} tabIndex={0}>
-                <b>Action Surges:</b> {classSpecific.action_surges}/<HiddenInput handleInputToggle={handleActionSurgesToggle} handleValueChange={(value) => handleActionSurgesChange(value)} showInput={showActionSurgesInput} value={actionSurges}></HiddenInput> <span className="text-muted">(max/cur)</span>
-            </div>
-            <div className="clickable" onClick={handleIndomitableUsesToggle} onKeyDown={handleIndomitableUsesToggle} tabIndex={0}>
-                <b>Indomitable Uses:</b> {classSpecific.indomitable_uses}/<HiddenInput handleInputToggle={handleIndomitableUsesToggle} handleValueChange={(value) => handleIndomitableUsesChange(value)} showInput={showIndomitableUsesInput} value={indomitableUses}></HiddenInput> <span className="text-muted">(max/cur)</span>
-            </div>
-        </div>}
-    </React.Fragment>)
+    const classSpecific = playerStats.class?.class_levels?.[playerStats.level - 1]?.class_specific;
+        if (!classSpecific) {
+            return null;
+        }
+        return (<React.Fragment>
+             {playerStats.class.name === 'Fighter' && <div>
+                 <div><b>Fighting Styles: </b>{playerStats.class.fightingStyles.join(', ')}</div>
+                 {playerStats.class.subclass.maneuvers && <div>
+                     <b>Maneuvers: </b>{playerStats.class.subclass.maneuvers.sort().join(', ')}
+                 </div>}
+                 <div><b>Extra Attacks: </b>{classSpecific.extra_attacks}</div>
+                 <div className="clickable" onClick={handleActionSurgesToggle} onKeyDown={handleActionSurgesToggle} tabIndex={0}>
+                     <b>Action Surges:</b> {classSpecific.action_surges}/<HiddenInput handleInputToggle={handleActionSurgesToggle} handleValueChange={(value) => handleActionSurgesChange(value)} showInput={showActionSurgesInput} value={actionSurges}></HiddenInput> <span className="text-muted">(max/cur)</span>
+                 </div>
+                 <div className="clickable" onClick={handleIndomitableUsesToggle} onKeyDown={handleIndomitableUsesToggle} tabIndex={0}>
+                     <b>Indomitable Uses:</b> {classSpecific.indomitable_uses}/<HiddenInput handleInputToggle={handleIndomitableUsesToggle} handleValueChange={(value) => handleIndomitableUsesChange(value)} showInput={showIndomitableUsesInput} value={indomitableUses}></HiddenInput> <span className="text-muted">(max/cur)</span>
+                 </div>
+             </div>}
+         </React.Fragment>)
 }
 
 export default CharClassFighter
