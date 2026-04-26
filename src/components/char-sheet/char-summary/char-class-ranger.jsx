8cf4f358158ed6c7de50c1cd01830024095bf445
paulgilchrist <PGilchrist@pulte.com>
2026-04-26 12:54:51 -0400

    Update test data

--- src/components/char-sheet/char-summary/char-class-ranger.jsx
+++ src/components/char-sheet/char-summary/char-class-ranger.jsx
@@ -10,35 +10,15 @@ function CharClassRanger({ playerStats }) {
         extraAttacks = 1;
     }
     
-    let favoredEnemiesCount;
-    let favoredEnemiesDisplay;
+    const favoredEnemiesCount = classRules.getFavoredEnemy(playerStats);
     
-    if (is2024) {
-        favoredEnemiesCount = classRules.getFavoredEnemy(playerStats);
-        favoredEnemiesDisplay = favoredEnemiesCount;
-     } else {
-        const classSpecific = playerStats.class.class_levels[playerStats.level-1].class_specific;
-        favoredEnemiesCount = classSpecific.favored_enemies;
-        favoredEnemiesDisplay = classSpecific.favored_enemies;
-     }
-    
-    let favoredTerrainCount;
-    let favoredTerrainDisplay;
-    
-    if (!is2024) {
-        const classSpecific = playerStats.class.class_levels[playerStats.level-1].class_specific;
-        favoredTerrainCount = classSpecific.favored_terrain;
-        favoredTerrainDisplay = classSpecific.favored_terrain;
-     }
-    
-    return (<React.Fragment>
-          {playerStats.class.name === 'Ranger' && <div>
-              <div><b>Fighting Styles: </b>{playerStats.level > 1 ? playerStats.class.fightingStyles.join(', ') : ''}</div>
-              <div><b>Extra Attacks: </b>{extraAttacks}</div>
-              <div><b>Favored Enemies: </b>{favoredEnemiesDisplay} - {playerStats.class.favoredEnemies.join(',')}</div>
-              {!is2024 && <div><b>Favored Terrain: </b>{favoredTerrainDisplay} - {playerStats.class.favoredTerrain.join(',')}</div>}            
-          </div>}
-      </React.Fragment>)
+        return (<React.Fragment>
+               {playerStats.class.name === 'Ranger' && <div>
+                   <div><b>Fighting Styles: </b>{playerStats.level > 1 ? playerStats.class.fightingStyles.join(', ') : ''}</div>
+                   <div><b>Extra Attacks: </b>{extraAttacks}</div>
+                   <div><b>Favored Enemies: </b>{favoredEnemiesCount}</div>
+               </div>}
+           </React.Fragment>)
 }
 
 export default CharClassRanger
