201ee0a7f55a942c14848b4b0ca608fc985b0d71
paulgilchrist <PGilchrist@pulte.com>
2026-04-26 12:21:27 -0400

    Add channel_divinity support for 2024 Paladin class

--- src/components/char-sheet/char-summary/char-class-paladin.jsx
+++ src/components/char-sheet/char-summary/char-class-paladin.jsx
@@ -2,18 +2,24 @@
 import React from 'react'
 
 function CharClassPaladin({ playerStats }) {
-    const classSpecific = playerStats.class.class_levels[playerStats.level-1].class_specific;
-    let extraAttacks = 0;
-    if(playerStats.level > 4) { // "Extra Attack" class feature at level 5
-        extraAttacks = 1;
-    }
-    return (<React.Fragment>
-        {playerStats.class.name === 'Paladin' && <div>
-            <div><b>Fighting Styles: </b>{playerStats.class.fightingStyles.join(', ')}</div>
-            <div><b>Extra Attacks: </b>{extraAttacks}</div>
-            <div><b>Aura Range: </b>{classSpecific.aura_range}</div>
-        </div>}
-    </React.Fragment>)
+        const classLevel = playerStats.class.class_levels[playerStats.level-1];
+        const classSpecific = classLevel.class_specific || {};
+          // 2024 uses top-level channel_divinity, 5e uses class_specific.channel_divinity_charges
+        const channelDivinity = playerStats.rules === '2024'
+              ? classLevel.channel_divinity
+              : classSpecific.channel_divinity_charges;
+        let extraAttacks = 0;
+        if(playerStats.level > 4) { // "Extra Attack" class feature at level 5
+            extraAttacks = 1;
+         }
+        return (<React.Fragment>
+               {playerStats.class.name === 'Paladin' && <div>
+                   <div><b>Fighting Styles: </b>{playerStats.class.fightingStyles.join(', ')}</div>
+                   <div><b>Extra Attacks: </b>{extraAttacks}</div>
+                   <div><b>Channel Divinity: </b>{channelDivinity}</div>
+                   {playerStats.rules !== '2024' && <div><b>Aura Range: </b>{classSpecific.aura_range}</div>}
+               </div>}
+           </React.Fragment>)
 }
 
 export default CharClassPaladin
